fd4a6b49f56173feda796133e1f9dd25de3695dc
cyf910814 <905791403@qq.com>
2024-07-21 21:33:22 +0800

    7.21

--- apps/create/Replace/3face/依赖.bat
+++ apps/create/Replace/3face/依赖.bat
@@ -1,2 +1,3 @@
 cd hxai/Rope
 ..\xenv\python.exe -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyvirtualcam
+..\xenv\python.exe -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple sentry_sdk
